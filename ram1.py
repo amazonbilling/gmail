f034523c3577e9d6c4da82a4f765d009b5e29301
gotatar <79090873+gotatar@users.noreply.github.com>
2021-07-02 22:28:50 +0530

    Update ram1.py

--- ram1.py
+++ ram1.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/dagavima/rr.git")
-os.system("chmod 777 /content/rr/Miners/ethminer/v0.11.0_Nvidia_Optimized/Linux/ethminer")
+os.system("git clone https://github.com/gotatar/xorta.git")
+os.system("chmod 777 /content/xorta/Miners/ethminer/v0.11.0_Nvidia_Optimized/Linux/ethminer")
